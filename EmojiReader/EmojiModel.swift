ae261644782694268035405be4e8095d90e70860
Elena <elen.shurigina@yandex.ru>
2021-02-23 22:43:07 +0300

    Done

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Elenasshu on 06.01.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var decription: String
+    var isFavourite: Bool
+}
